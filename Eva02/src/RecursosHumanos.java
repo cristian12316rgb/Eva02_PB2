e6c3406525c4252e2015aa92c18d4af04bbfa160
mariano ledesma <marianoledesma2001@gmail.com>
2020-11-03 19:42:44 -0300

    cree la clase Empresa y lo instancie en la clase RecursosHumanos

--- Eva02/src/RecursosHumanos.java
+++ Eva02/src/RecursosHumanos.java
@@ -1,8 +1,13 @@
 import java.util.ArrayList;
 
-public class RecursosHumanos {
+public class RecursosHumanos extends Empresa{
 
     ArrayList<Empleado> empleados=new ArrayList<Empleado>();
+
+    public RecursosHumanos(String nombre) {
+        super(nombre);
+        this.empleados = empleados;
+    }
 
     public void despedirEmpleado(Empleado empleadoADespedir){
             empleados.remove(empleadoADespedir);
